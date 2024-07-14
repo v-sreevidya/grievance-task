a0adf4704ec0e88adccb9174d0f415c02fb3e9b0
v-sreevidya <sreevidyavijayan14@gmail.com>
2024-07-14 11:28:17 +0530

    adding greivance registration page 1

--- page1java.js
+++
@@ -1,7 +0,0 @@
-function updatePlaceholder(){
-    var dropdown=document.getElementById('reason');
-    var selectedOption=dropdown.options[dropdown.selectedIndex];
-    if(selectedOption.value==''){
-        selectedOption.style.display='none';
-    }
-}
