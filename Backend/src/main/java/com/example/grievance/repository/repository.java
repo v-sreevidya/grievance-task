8a4cdd3259d1b88d9174224cae711ee742ff7608
shreejknair <134157274+shreejknair@users.noreply.github.com>
2024-09-05 19:27:07 +0300

    Update repository.java

--- Backend/src/main/java/com/example/grievance/repository/repository.java
+++ Backend/src/main/java/com/example/grievance/repository/repository.java
@@ -1,9 +1,14 @@
-package com.example.grievance.repository;
+package com.example.grievance.Repository;
 
 import com.example.grievance.Entity.Grievance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface repository extends JpaRepository<Grievance,Integer> {
+public interface GrievanceRepository extends JpaRepository<Grievance, String> {
+
+    // Custom method to find grievance by ticket_no
+    Optional<Grievance> findByTicketNo(String ticketNo);
 }
