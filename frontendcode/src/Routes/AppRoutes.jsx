80a836577d9fb04fee26f814669f04b34e42eb64
ajusajeev111 <148002681+ajusajeev111@users.noreply.github.com>
2024-09-06 22:00:31 +0530

    Update AppRoutes.jsx

--- frontendcode/src/Routes/AppRoutes.jsx
+++ frontendcode/src/Routes/AppRoutes.jsx
@@ -7,24 +7,35 @@ import SignUp from '../Pages/SignUp/SignUp';
 import SignUp2 from '../Pages/SignUp2/SignUp2';
 import DashboardSupervisor from '../Pages/Supervisor/Dashboard/DashboardSupervisor';
 import Grievances from '../Pages/User/Grievances/Grievances';
-import Details from '../Pages/Details/Details';
-import DashboardLayout from '../Layout/DashboardLayout/DashboardLayout';
+import UserDashboardLayout from '../Layout/UserDashboardLayout/UserDashboardLayout';
+import SupervisorDashboardLayout from '../Layout/SupervisorDashboardLayout/SupervisorDashboardLayout';
+import DashboardLayout from '../Layout/UserDashboardLayout/UserDashboardLayout';
 import GrievanceRegistration from '../Pages/User/GrievanceRegistration/GrievanceRegistration';
 import GrievanceDetails from '../Pages/Supervisor/EditGrievance/GrievanceDetails';
+import GrievanceDetailsAssignee from '../Pages/Assignee/EditGrievance/GrievanceDetailsAssignee';
 import NotificationUser from '../Pages/User/NotificationUser/NotificationUser';
+import NotificationSupervisor from '../Pages//Supervisor/NotificationSupervisor/NotificationSupervisor';
+import DashboardAssignee from '../Pages/Assignee/DashBoard/DashboardAssignee';
+import NotificationAssignee from '../Pages/Assignee/NotificationAssignee/NotificationAssignee';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/SignUp" element={<SignUp />} />
-      <Route path="/SignUp2" element={<SignUp2 />} />
-      <Route path="/dashboard/supervisor" element={<DashboardLayout><DashboardSupervisor /></DashboardLayout>} />
-      <Route path="/dashboard/supervisor/edit" element={<DashboardLayout><GrievanceDetails /></DashboardLayout>} />
-      <Route path="/grievances" element={<DashboardLayout><Grievances /></DashboardLayout>} />
-      <Route path="/grievanceregistration" element={<DashboardLayout><GrievanceRegistration /></DashboardLayout>} />
-      <Route path="/details" element={<DashboardLayout><Details /></DashboardLayout>} />
-      <Route path="/notificationuser" element={<DashboardLayout><NotificationUser/></DashboardLayout>} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/signUp2" element={<SignUp2 />} />
+      {/* SUPERVISOR */}
+      <Route path="/dashboard/supervisor" element={<SupervisorDashboardLayout><DashboardSupervisor /></SupervisorDashboardLayout>} />
+      <Route path="/dashboard/supervisor/edit" element={<SupervisorDashboardLayout><GrievanceDetails /></SupervisorDashboardLayout>} />
+      <Route path="/notificationSupervisor" element={<UserDashboardLayout><NotificationSupervisor/></UserDashboardLayout>} />
+      {/* ASSIGNEE */}
+      <Route path="/dashboard/assignee" element={<DashboardLayout><DashboardAssignee /></DashboardLayout>} />
+      <Route path="/dashboard/assignee/edit" element={<DashboardLayout><GrievanceDetailsAssignee /></DashboardLayout>} />
+      <Route path="/notificationAssignee" element={<UserDashboardLayout><NotificationAssignee/></UserDashboardLayout>} />
+      {/* USER */}
+      <Route path="/grievances" element={<UserDashboardLayout><Grievances /></UserDashboardLayout>} />
+      <Route path="/grievanceregistration" element={<UserDashboardLayout><GrievanceRegistration /></UserDashboardLayout>} />
+      <Route path="/notificationuser" element={<UserDashboardLayout><NotificationUser/></UserDashboardLayout>} />
 
     </Routes>
   );
