77011de5286aa101cfac7605b34511b7a35c0946
shreejknair <134157274+shreejknair@users.noreply.github.com>
2024-09-08 17:31:01 +0300

    Update Notification.java

--- Backend/src/main/java/com/example/grievance/Entity/Notification.java
+++ Backend/src/main/java/com/example/grievance/Entity/Notification.java
@@ -1,1 +1,21 @@
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String ticketNumber;
+
+    private String message;
+    private String recipient;  // supervisor email or ID
+    private LocalDateTime sentAt;
+
+    // Constructor
+    public Notification(String message, String recipient) {
+        this.message = message;
+        this.recipient = recipient;
+        this.sentAt = LocalDateTime.now();
+    }
+
+    
+}
 
