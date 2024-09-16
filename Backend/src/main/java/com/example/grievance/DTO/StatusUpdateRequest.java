17b8d1bbec63a2d10b79504fac766c40f8afa5e0
ajusajeev111 <148002681+ajusajeev111@users.noreply.github.com>
2024-09-16 16:19:13 +0530

    Create StatusUpdateRequest.java

---
+++ Backend/src/main/java/com/example/grievance/DTO/StatusUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.grievance.DTO;
+
+public class StatusUpdateRequest {
+
+    private String status;
+
+    // Getter and Setter
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
