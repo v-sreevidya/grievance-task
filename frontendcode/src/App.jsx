002e24211be62ec7f77dde7dbf5f857593a25633
ajusajeev111 <148002681+ajusajeev111@users.noreply.github.com>
2024-09-16 16:52:47 +0530

    Update App.jsx

--- frontendcode/src/App.jsx
+++ frontendcode/src/App.jsx
@@ -2,13 +2,16 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './Routes/AppRoutes';
+import { FormProvider } from './FormContext/FormContext';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <FormProvider>
         <AppRoutes/>
+      </FormProvider>
       </BrowserRouter>
     </div>
   );
